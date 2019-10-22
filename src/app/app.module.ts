95b29ff056d0596d6078c95f2cdc7885e8887994
Rajesh Pabbu <rajeshpabbu548@gmail.com>
2019-10-22 11:17:34 -0400

    Teamplate Setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,15 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { FormsModule } from "@angular/forms";
 
-import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
+import { AppComponent } from "./app.component";
+import { HelloComponent } from "./hello.component";
+import { NavComponent } from "./nav/nav.component";
+import { ContentComponent } from "./content/content.component";
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [BrowserModule, FormsModule],
+  declarations: [AppComponent, HelloComponent, NavComponent, ContentComponent],
+  bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
